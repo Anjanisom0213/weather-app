66558c59fc1805994fe3ad0bb280775a971bec7e
Anjani Som <anjanisom72@gmail.com>
2025-05-03 10:29:57 +0530

    new chnages

--- app.py
+++ app.py
@@ -29,13 +29,50 @@ def handle_weather_request(city):
     print("📦 Response Code:", response.status_code)
     print("📄 Response Body:", response.text)
 
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     emit('weather_response', {
+    #         'temperature': data['main']['temp'],
+    #         'weather': data['weather'][0]['description'],
+    #         'city': data['name']
+    #     })
+    # else:
+    #     emit('weather_response', {
+    #         'error': 'City not found or API error.'
+    #     })
+
     if response.status_code == 200:
         data = response.json()
-        emit('weather_response', {
-            'temperature': data['main']['temp'],
-            'weather': data['weather'][0]['description'],
-            'city': data['name']
-        })
+
+        # weather_info = {
+        #     'temperature': data['main']['temp'],
+        #     # 'weather': data['weather'][0]['description'],
+        #     'description': data['weather'][0]['description'],
+        #     'humidity': data['main']['humidity'],
+        #     'pressure': data['main']['pressure'],
+        #     'city': data['name']
+        # }
+
+        weather_info = {
+              'temperature': data['main']['temp'],
+              'feels_like': data['main']['feels_like'],
+              'description': data['weather'][0]['description'],
+              'icon': data['weather'][0]['icon'],
+              'humidity': data['main']['humidity'],
+              'pressure': data['main']['pressure'],
+              'wind_speed': data['wind']['speed'],
+              'sunrise': data['sys']['sunrise'],
+              'sunset': data['sys']['sunset'],
+              'city': data['name'],
+              'country': data['sys']['country']
+        }
+
+
+        print(weather_info)
+
+
+        emit('weather_response', weather_info)
+
     else:
         emit('weather_response', {
             'error': 'City not found or API error.'
